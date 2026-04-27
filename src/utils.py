f2aa92c296befa6ebaf0072b97978952175947bb
yatorch <yousiffythalmeshari@gmail.com>
2026-04-27 21:19:41 +0300

    fixed util

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 # Utility funcs
 
 import datetime as dt
+from datetime import timezone
 import pandas_market_calendars as mcal
 import pandas as pd
 
@@ -11,8 +12,15 @@ def get_latest_mclose_date():
     start_date_nd = date_today_raw - dt.timedelta(days=7)
     nasdaq_schedule = nasdaq_calendar.schedule(start_date=start_date_nd, end_date=date_today_raw)
     print(f"last market close: {nasdaq_schedule['market_close'].iloc[-1]}")
+    print(f"last market open: {nasdaq_schedule['market_open'].iloc[-1]}")
+    now = dt.datetime.now(timezone.utc)
+    chour = now.hour
+    cmin = now.minute
+    csec = now.second
+    
+    print(f"current time: {pd.Timestamp(now, hour=chour, minute=cmin, second=csec)}")
 
-    if pd.Timestamp(dt.date.today(), tz='UTC') < nasdaq_schedule['market_close'].iloc[-1]:
+    if pd.Timestamp(now) < nasdaq_schedule['market_open'].iloc[-1]:
         date_today = dt.datetime.combine(nasdaq_schedule.index[-2].date(), dt.time.min)
         return date_today
     else:
